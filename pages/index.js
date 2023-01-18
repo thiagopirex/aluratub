17ac028a16cb071a2499ea82ba030b7043c01404
thiagopirex@gmail.com <thiagopirex@gmail.com>
2023-01-18 10:55:54 -0300

    Correcting keys for ReactJS

--- pages/index.js
+++ pages/index.js
@@ -31,13 +31,13 @@ function Timeline(props) {
             {playlistNames.map((playlistName) => {
                 const video = props.playlists[playlistName];
                 return (
-                    <section>
+                    <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
                             {
                                 video.map((video) => {
                                     return (
-                                        <a href={video.url}>
+                                        <a key={video.url} href={video.url}>
                                             <img src={video.thumb}></img>
                                             <span>
                                                 {video.title}
